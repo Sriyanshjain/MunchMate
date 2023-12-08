99f1c4e892b9c50665d99ed7b68e83ed5ef533dd
sriyanshjain <shreyanshjain036@gmail.com>
2023-12-08 09:33:03 +0530

    adding index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+import Cart from "./src/components/Cart";
+import Shimmer from "./src/components/Shimmer";
+import { Body } from "./src/components/Body";
+import ReactDOM from "react-dom/client";
+import { lazy, Suspense, useContext } from "react"
+import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom";
+import Error from "./src/components/Error";
+import ContactUs from "./src/components/ContactUs";
+import About from "./src/components/About"
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import App from "./src/App";
+const Grocery=lazy(()=>{
+    return  import("./src/components/Grocery")
+ })
+ const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>,
+                
+            },
+            {
+                path:"/about",
+                element:<About/>,
+                
+            },
+            {
+                path:"/contact",
+                element:<ContactUs/>
+            },
+            {
+                path:"/restaurant/:resId",
+                element:<RestaurantMenu/>
+            },
+            {
+                path:"/cart",
+                element:<Cart/>
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<Shimmer/>}><Grocery/></Suspense>
+            }
+        ],
+        errorElement:<Error/>
+    }
+ 
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter}/>)
--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { lazy, Suspense, useContext } from "react";
-import ReactDOM from "react-dom/client";
+import React from "react";
+
 import {Header} from "./components/Header"
-import { Body } from "./components/Body";
-import Error from "./components/Error";
-import ContactUs from "./components/ContactUs";
-import About from "./components/About"
-import RestaurantMenu from "./components/RestaurantMenu";
-import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom";
-import Shimmer from "./components/Shimmer";
+
+import { Outlet } from "react-router-dom";
+
 import 'keen-slider/keen-slider.min.css'
 import { UserContext } from "./utils/UserContext";
 import { Provider } from "react-redux";
-import Cart from "./components/Cart";
+
 import appStore from "./utils/store/appStore";
 //Chunking
 //Lazy-loading
@@ -19,11 +15,9 @@ import appStore from "./utils/store/appStore";
 //Dynamic Bundling
 //On demand loading
 //dynamic importing
-const Grocery=lazy(()=>{
-   return  import("./components/Grocery")
-})
 
-const AppLayout=()=>{
+
+const App=()=>{
     
     return (
         <Provider store={appStore}>
@@ -38,41 +32,4 @@ const AppLayout=()=>{
         </Provider>
     )
 }
-const appRouter=createBrowserRouter([
-    {
-        path:"/",
-        element:<AppLayout/>,
-        children:[
-            {
-                path:"/",
-                element:<Body/>,
-                
-            },
-            {
-                path:"/about",
-                element:<About/>,
-                
-            },
-            {
-                path:"/contact",
-                element:<ContactUs/>
-            },
-            {
-                path:"/restaurant/:resId",
-                element:<RestaurantMenu/>
-            },
-            {
-                path:"/cart",
-                element:<Cart/>
-            },
-            {
-                path:"/grocery",
-                element:<Suspense fallback={<Shimmer/>}><Grocery/></Suspense>
-            }
-        ],
-        errorElement:<Error/>
-    }
- 
-])
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/>)
+export default App;
