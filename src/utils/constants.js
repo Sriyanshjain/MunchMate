e17566cfda9b043c64fc1f77987f6e2e171912d2
sriyanshjain <shreyanshjain036@gmail.com>
2023-12-10 14:39:58 +0530

    using new APIs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,3 @@
-export const CDN_URL = "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
-export const LOGO_URL = "https://corsproxy.io/?https://logolook.net/wp-content/uploads/2023/04/Swiggy-Logo.png";
-
-export const MENU_URL="https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
-
-export const GET_RESATAURANT="https://corsproxy.io/?https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-
-// export const CDN_URL ="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
-// export const LOGO_URL =
-// "https://logolook.net/wp-content/uploads/2023/04/Swiggy-Logo.png";
-
-// export const MENU_URL=
-// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
-
-// export const GET_RESATAURANT="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const MENU_URL="https://munchmate-server.onrender.com/api/menu?lat=12.9715987&lng=77.5945627&restaurantId="
+export const GET_RESATAURANT="https://munchmate-server.onrender.com/api/restaurants?lat=12.9715987&lng=77.5945627"
