87e1fed1f672fb251b096b029cf1276eefbe39b4
Sriyansh Jain <37472157+Sriyanshjain@users.noreply.github.com>
2026-02-28 23:04:09 +0530

    Refactor useRestaurantMenu to use mock data
    Refactor useRestaurantMenu to use mock data and remove API calls.

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,49 +1,20 @@
-import { useEffect,useState } from "react";
-import { FREE_MENU_URL, MENU_URL } from "./constants";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import menuData from "../mockData/menuData.json";
 
-const useRestaurantMenu=(resId)=>{
-
-    const address=useSelector((store) => store.location.address);
-    const[resInfo,setResInfo]=useState(null);
-    
+const useRestaurantMenu = (resId) => {
+    const [resInfo, setResInfo] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    useEffect(()=>{
-        fetchMenu()
-    },[]);
-    const fetchMenu=async()=>
-    {
-        try{
-            setIsLoading(true);
-            const data= await fetch(MENU_URL+'?lat='+`${address.latitude}`+'&lng='+`${address.longitude}`+'&restaurantId='+resId + '&catalog_qa=undefined'+'&submitAction=ENTER')
-            const usefulData=await data.json();
-            setResInfo(usefulData.data);
-            
-            
-        }
-     catch(error)
-     {
-       // console.log("an error has occured"+ error);
-       try{
+
+    useEffect(() => {
         setIsLoading(true);
-        const data= await fetch(FREE_MENU_URL+'?lat='+`${address.latitude}`+'&lng='+`${address.longitude}`+'&restaurantId='+resId+'&catalog_qa=undefined'+'&submitAction=ENTER')
-        const usefulData=await data.json();
-        setResInfo(usefulData.data);
-        
-        
-    }
-    catch(error)
-    {
-        setError(error.response);
-    }
-       
-     }
-     finally {
-        setIsLoading(false);
-      }
-      }
-   return {resInfo,isLoading,error};
-    
-}
+        setTimeout(() => {
+            setResInfo(menuData.data);
+            setIsLoading(false);
+        }, 300);
+    }, [resId]);
+
+    return { resInfo, isLoading, error };
+};
+
 export default useRestaurantMenu;
